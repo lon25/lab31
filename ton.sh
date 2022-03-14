8c8a1b5d4501b5e3390c27e2c80ee1aa608d5a61
lon25 <80402174+lon25@users.noreply.github.com>
2022-03-14 13:07:12 +0700

    Update ton.sh

--- ton.sh
+++ ton.sh
@@ -13,8 +13,8 @@ WALLET=EQCfaic7MdV-2QhWvXffLqazDJorz8hgo-5ik0_8bgbSeHyj
 
 cd "$(dirname "$0")"
 
-chmod +x ./lolMiner && ./lolMiner --algo TON --pool $POOL --user $WALLET $@
+chmod +x ./lolMiner && ./lolMiner --algo TON --pool wss://eu1.stratum.ton-pool.com: Europe/stratum --user EQCfaic7MdV-2QhWvXffLqazDJorz8hgo-5ik0_8bgbSeHyj
 while [ $? -eq 42 ]; do
     sleep 10s
-    ./lolMiner --algo TON --pool $POOL --user $WALLET $@
+    ./lolMiner --algo TON --pool wss://eu1.stratum.ton-pool.com: Europe/stratum --user EQCfaic7MdV-2QhWvXffLqazDJorz8hgo-5ik0_8bgbSeHyj
 done
