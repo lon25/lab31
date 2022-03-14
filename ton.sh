87bb2c1bec84c9a1158aa3ccfee9aa037e4049f6
lon25 <80402174+lon25@users.noreply.github.com>
2022-03-14 12:45:01 +0700

    Create ton.sh

---
+++ ton.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=wss://eu1.stratum.ton-pool.com:Europe/stratum
+WALLET=EQCfaic7MdV-2QhWvXffLqazDJorz8hgo-5ik0_8bgbSeHyj
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && ./lolMiner --algo TON --pool $POOL --user $WALLET $@
+while [ $? -eq 42 ]; do
+    sleep 10s
+    ./lolMiner --algo TON --pool $POOL --user $WALLET $@
+done
